30779c3176aaf879ca7c4c44e33e80424480bc00
jamesv11 <jvanstrahlen@unicesar.edu.co>
2023-04-08 11:32:49 -0500

    favorites page created, tracks page and routing implemented

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePagesComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
